ad28636077fa37bbf11bc059a7b27d4a6b50b3a8
Logan <S>
2019-11-25 20:09:08 -0600

    "Test1"

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * Created by LaunchCode
  */
 public class TechJobs {
-
+System.out.println("Test");
     private static Scanner in = new Scanner(System.in);
 
     public static void main (String[] args) {
